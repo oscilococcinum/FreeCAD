d32a0a96bf2fd13640d504cf6a4cfad117995e5b
oscilococcinum <bargaw28@gmail.com>
2024-12-23 15:03:28 +0100

    adds nix devshell

--- package/nix/flake.nix
+++ package/nix/flake.nix
@@ -1,17 +1,31 @@
+#This flake is intentionaly impure.
+#To build freecad-dev from source add --impure at the end of nix build
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map (system: 
-      {
-        name = system;
-        value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
-          
-          freecad-build = pkgs.callPackage (import ./freecad/package.nix) { };
+    packages = builtins.listToAttrs (map (system: {
+      name = system;
+      value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
+        freecad-build = pkgs.callPackage (import ./freecad/package.nix) { };
+        default = freecad-build;
+      };
+    })[ "x86_64-linux" "aarch64-linux" ]);
+
+    devShells = builtins.listToAttrs (map (system: {
+      name = system;
+      value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
+        freecad-devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            calculix
+            gmsh
+          ];
+          shellHook = '''';
         };
-      }
-    )[ "x86_64-linux" "aarch64-linux" ]);
+        default = freecad-devShell;
+      };
+    })[ "x86_64-linux" "aarch64-linux" ]);
   };
 }
