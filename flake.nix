1091f4ab461ed6772b33fd77c63729a08e31121a
oscilococcinum <bargaw28@gmail.com>
2025-02-15 23:40:36 +0100

    refactoring v2

--- flake.nix
+++ flake.nix
@@ -16,6 +16,14 @@
       submodules = true;
       flake = false;
     };
+    freecad-src = {
+      type = "git";
+      url = "https://github.com/FreeCAD/FreeCAD";
+      ref = "main";
+      submodules = true;
+      flake = false;
+    };
+
   };
 
   outputs = { nixpkgs, ... }@inputs: {
@@ -23,11 +31,13 @@
         name = system;
         value = with import nixpkgs { inherit system; config.allowUnfree = true;}; rec {
 
-          freecad-appimage = pkgs.callPackage (import ./package/nix/weekly.nix) { src = inputs.appimage-file-weekly; };
+          freecad-appimage = pkgs.callPackage (import ./package/nix/appimage-default.nix) { src = inputs.appimage-file-weekly;  pname = "freecad-weekly"; version = "weekly"; };
 
-          freecadrt-appimage = pkgs.callPackage (import ./package/nix/realthunder.nix) { src = inputs.appimage-file-rt; pname = "freecad-weekly"; version = "dev"; };
+          freecadrt-appimage = pkgs.callPackage (import ./package/nix/appimage-default.nix) { src = inputs.appimage-file-rt; pname = "freecad-rt"; version = "stable"; };
 
           astocad = pkgs.callPackage (import ./package/nix/default.nix) { src = inputs.astocad-src; pname = "astocad"; version = "dev"; };
+
+          freecad = pkgs.callPackage (import ./package/nix/default.nix) { src = inputs.freecad-src; pname = "freecad"; version = "dev"; };
 
           default = freecad-appimage;
         };
